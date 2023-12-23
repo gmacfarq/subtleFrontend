b6497ae60c97c35df3417cf04bd03770909f29fc
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-12-23 15:52:24 -0800

    gallery images

--- src/components/Press.js
+++ src/components/Press.js
@@ -18,21 +18,21 @@ function Press() {
         <h1>Subtle Orange in the Press</h1>
       </div>
       <div className='press-record'>
-        <img className='press-image' src={`./images/fash.png`} alt="fash" />
+        <img className='press-image' src={`./images/fash.webp`} alt="fash" />
         <div className='text'>
           <h2><a href='https://www.fashionably-early.com/2023/11/20/subtle-orange-easier-said/' rel="noreferrer" target="_blank">Fashionably Early</a></h2>
           <p>Article - Easier Said</p>
         </div>
       </div>
       <div className='press-record'>
-        <img className='press-image' src={`./images/scratch.png`} alt="scratch" />
+        <img className='press-image' src={`./images/scratch.webp`} alt="scratch" />
         <div className='text'>
           <h2><a href='https://www.scratch-zine.com/product/scratch-magazine-vol-3' rel="noreferrer" target="_blank">Scratch</a></h2>
           <p>Vol. 3</p>
         </div>
       </div>
       <div className='press-record'>
-        <img className='press-image' src={`./images/offthecassette.jpeg`} alt="scratch" />
+        <img className='press-image' src={`./images/offthecassette.webp`} alt="scratch" />
         <div className='text'>
           <h2>Off The Cassette</h2>
           <p>Artist Spotlight</p>
